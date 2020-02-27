e22ca2f1a5e03f188ed4220fbb03e71eb4d45ddb
Marco Gonzalez <marcgonz@yahoo.com>
2020-02-27 09:27:01 -0600

    Clase

--- Clases/Clase_12.R
+++ Clases/Clase_12.R
@@ -1,7 +1,28 @@
 
 # Importar datos de CedroRojo ---------------------------------------------
 
-CR <- read.csv("Clases/CedroRojo.csv", header = T)
+CR <- read.csv("CedroRojo.csv", header = T)
 
 
 summary(CR)
+
+
+
+# Una Muestra -------------------------------------------------------------
+
+# Media teorética de la variable "Diametro"
+# Establecida por CONAFOR para cedro rojo es igual a 13
+
+#  Comparar la media observada de la variable diámetro para
+#  las plántulas producidas en vivero (media=12.524, valores
+#  observados).
+
+# "mu" debe ser igual a la variable teorética
+#  el valor de alfa establecido es 0.05
+
+t.test(CR$diametro, mu=12.7)
+
+
+t.test(CR$altura, mu = 19)
+
+
